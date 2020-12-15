9a76605cc79caac8742a2837b99f75c2deafa5a6
wassilva <wash.bsilva@gmail.com>
2020-12-15 14:15:11 -0300

    footer

---
+++ views/js/footer.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+    var html =  '<footer class="footer bg-dark">'
+    html +=         '<div class="container text-center"><span class="text-light">&copy; Copyright 2020 Washington Dealership - All Rights Reserved</span></div>'
+    html +=     '</footer>'
+    $("#footer").append(html)
+})
