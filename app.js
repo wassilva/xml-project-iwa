1695f04407bd865e94d76b43673b627e00f4f50c
wassilva <wash.bsilva@gmail.com>
2020-12-19 17:40:42 -0300

    add server side of cart

--- app.js
+++ app.js
@@ -20,16 +20,16 @@ function xmlFileToJs(filename, cb) {
       if (err) throw (err);
       xml2js.parseString(xmlStr, {}, cb);
     });
-  }
+}
   
-  //Function to convert JSON to XML and save it
-  function jsToXmlFile(filename, obj, cb) {
+//Function to convert JSON to XML and save it
+function jsToXmlFile(filename, obj, cb) {
     var filepath = path.normalize(path.join(__dirname, filename));
     var builder = new xml2js.Builder();
     var xml = builder.buildObject(obj);
     fs.unlinkSync(filepath);
     fs.writeFile(filepath, xml, cb);
-  }
+}
 
 router.get('/', function(req, res) {
 
@@ -37,11 +37,12 @@ router.get('/', function(req, res) {
 
 });
 
+
 router.get('/get/car', function(req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
 
-    var xml = fs.readFileSync('Dealership.xml', 'utf8'); //We are reading in the XML file
+    var xml = fs.readFileSync('car.xml', 'utf8'); //We are reading in the XML file
     var xsl = fs.readFileSync('car.xsl', 'utf8'); //We are reading in the XSL file
 
     var doc = xmlParse(xml); //Parsing our XML file
@@ -52,11 +53,12 @@ router.get('/get/car', function(req, res) {
     res.end(result.toString()); //Send the result back to the user, but convert to type string first
 
 });
+
 router.get('/get/motorcycle', function(req, res) {
 
     res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
 
-    var xml = fs.readFileSync('Dealership.xml', 'utf8'); //We are reading in the XML file
+    var xml = fs.readFileSync('motorcycle.xml', 'utf8'); //We are reading in the XML file
     var xsl = fs.readFileSync('motorcycle.xsl', 'utf8'); //We are reading in the XSL file
 
     var doc = xmlParse(xml); //Parsing our XML file
@@ -71,19 +73,21 @@ router.get('/get/motorcycle', function(req, res) {
 router.post('/post/json/add_car', function (req, res) {
 
     function appendJSON(obj) {
-
+        var status
         console.log(obj)
 
-        xmlFileToJs('Dealership.xml', function (err, result) {
+        xmlFileToJs('car.xml', function (err, result) {
             if (err) throw (err);
-            
             result.carmenu.section[obj.sec_add].entree.push({'item': obj.item_add, 'price': obj.price_add});
-
-            console.log(JSON.stringify(result, null, "  "));
-
-            jsToXmlFile('Dealership.xml', result, function(err){
-                if (err) console.log(err);
-            });
+            
+            if(obj.item_add == '' || obj.price_add == ''){
+            } 
+            else{
+                jsToXmlFile('car.xml', result, function(err){
+                    if (err) console.log(err);
+                })
+            }
+            
         });
     };
 
@@ -91,6 +95,7 @@ router.post('/post/json/add_car', function (req, res) {
 
     res.redirect('back');
 
+    
 });
 
 router.post('/post/json/rm_car', function (req, res) {
@@ -99,7 +104,7 @@ router.post('/post/json/rm_car', function (req, res) {
 
         console.log(obj)
 
-        xmlFileToJs('Dealership.xml', function (err, result) {
+        xmlFileToJs('car.xml', function (err, result) {
             if (err) throw (err);
             var item = obj.item_rm
             var confirmation = false
@@ -116,6 +121,7 @@ router.post('/post/json/rm_car', function (req, res) {
                     }
                     else{
                         confirmation = true
+                        erro = 1
                     }
                 }
                 catch(e){
@@ -126,22 +132,69 @@ router.post('/post/json/rm_car', function (req, res) {
                 
             }
             console.log(JSON.stringify(result, null, "  "));
-
-            jsToXmlFile('Dealership.xml', result, function(err){
-                if (err) console.log(err);
-            });
-        });
+            if(item == ""){
+            }
+            else{
+                if(erro != 1){
+                    jsToXmlFile('car.xml', result, function(err){
+                        if (err) console.log(err);
+                    });
+                }
+            }
+        })
     };
 
     appendJSON(req.body);
-    if(erro == 1){
-        res.redirect('back');
-        res.send("Errou na entrada!")
-    }
-    else{
-        res.redirect('back');
-    }
+
+    res.redirect('back');
+
 });
+
+router.post('/get/html/cart/car', function(req,res){
+    function getJson(obj){
+        console.log(obj)
+        xmlFileToJs('cart.xml', function(err2, result2){
+            if(err2) throw (err2)
+            
+            // console.log(JSON.stringify(result, null, "  "));
+            
+        
+            xmlFileToJs('car.xml', function(err1, result1){
+                if(err1) throw (err1)
+                var json_cart
+                var accessory_xml = result1.carmenu.section[2].entree
+                for(var i = 0 ; i < obj.accessory.length ; i++ ){
+                    console.log(Array.isArray(obj.accessory))
+                    for(var j = 0 ; j < accessory_xml.length ; j++ ){
+                        if(obj.accessory[i] == accessory_xml[j].item){
+                            json_cart = accessory_xml[j]
+                            result2.cart.section[0].entree.push({'item': json_cart.item, 'price': json_cart.price})
+                        }
+
+                    }
+                }
+                console.log(JSON.stringify(result2, null, "  "))
+                // if(obj.accessory)
+                //     var accessories = result.carmenu.section[obj.accessory]
+                jsToXmlFile('cart.xml', result2, function(err2){
+                    if(err2) console.log(err2)
+                })
+            })
+        })
+    }
+    getJson(req.body)
+    res.writeHead(200, {'Content-Type': 'text/html'}); //We are responding to the client that the content served back is HTML and the it exists (code 200)
+
+    var xml = fs.readFileSync('cart.xml', 'utf8'); //We are reading in the XML file
+    var xsl = fs.readFileSync('cart.xsl', 'utf8'); //We are reading in the XSL file
+
+    var doc = xmlParse(xml); //Parsing our XML file
+    var stylesheet = xmlParse(xsl); //Parsing our XSL file
+
+    var result = xsltProcess(doc, stylesheet); //This does our XSL Transformation
+
+    res.end(result.toString()); //Send the result back to the user, but convert to type string first
+})
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function () {
     var addr = server.address();
