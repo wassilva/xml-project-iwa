6df16195b1e7c5b798fb4bec1cd100312a36e940
wassilva <wash.bsilva@gmail.com>
2020-12-26 06:57:33 +0000

    validation of add to cart with json schema

---
+++ views/js/add_to_cart.js
@@ -0,0 +1,58 @@
+function add_to_cart(){
+    var choose = $('#urlGet').val()
+    if(choose == '/get/car'){
+        var car = $(".car:checked").val(),
+            model = $(".model:checked").val(),
+            door = $(".door:checked").val()
+            accessory = new Array();
+        $('input[name="accessory[]"]:checked').each(function(){
+            accessory.push($(this).val());
+        });
+        var data = {
+            car: car,
+            model: model,
+            accessory: accessory,
+            door: door
+        }
+    }
+    else{
+        var motorcycle = $(".motorcycle:checked").val(),
+            grips = $(".grips:checked").val(),
+            seat = $(".seat:checked").val()
+            helmets = new Array();
+        $('input[name="helmets[]"]:checked').each(function(){
+            helmets.push($(this).val());
+        });
+        var data = {
+            motorcycle: motorcycle,
+            helmets: helmets,
+            seat: seat,
+            grips: grips
+        }
+    }
+    
+    $.getJSONuncached = function(url) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: data,
+            cache: false,
+            success: function(html){
+                if (html != "invalid input!"){ 
+                    window.location.href = "/cart"
+                }
+                else {
+                    alert("Something went wrong when adding to cart")
+                }
+            }
+        })
+    }
+    var url = document.querySelector("#cart").getAttribute("name")
+    $.getJSONuncached(url)
+}
+
+$(document).ready(function(){
+    $('#btn-submit-cart').on("click", function(){
+        add_to_cart()
+    })
+})
