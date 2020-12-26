2e7097ec6162abcc7b6e9a95cf7b435e2d1bd1db
wassilva <wash.bsilva@gmail.com>
2020-12-26 06:59:52 +0000

    data management, validation with json schema

--- views/js/manager_list.js
+++ views/js/manager_list.js
@@ -1,30 +1,66 @@
 function check_add(){
-    var item_add = document.querySelector('#item_add').value
-    var price_add = document.querySelector('#price_add').value
-
-    if(item_add == "" || price_add == ""){
-        alert("There was an error adding the item!")
+    var sec_add = document.querySelector("#sec_add").value,
+        item_add = document.querySelector("#item_add").value,
+        price_add = document.querySelector("#price_add").value
+    $.getJSONuncached = function(url) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data:{
+                sec_add: sec_add,
+                item_add: item_add,
+                price_add: price_add
+            },
+            cache: false,
+            success: function(html){
+                if (html != "invalid input!"){ 
+                    alert("Item added successfully!")
+                }
+                else {
+                    alert("There was an error adding the item!")
+                }
+            }
+        })
     }
-    else{
-        alert("Item added successfully!")
-    }
+    var url = document.querySelector("#add").getAttribute("name")
+    $.getJSONuncached(url)
 }
 function check_rm(){
-    var item_rm = document.querySelector('#item_rm').value
-
-    if(item_rm == ""){
-        alert("There was an error removing the item!")
+    var sec_rm = document.querySelector("#sec_rm").value,
+        item_rm = document.querySelector("#item_rm").value
+    $.getJSONuncached = function(url) {
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data:{
+                sec_rm: sec_rm,
+                item_rm: item_rm
+            },
+            cache: false,
+            success: function(html){
+                if (html != "invalid input!"){ 
+                    alert("Item removed successfully!")
+                }
+                else {
+                    alert("There was an error removing the item!")
+                }
+            }
+        })
     }
-    else{
-        alert("Item adding successfully!")
-    }
-
+    var url = document.querySelector("#rm").getAttribute("name")
+    $.getJSONuncached(url)
 }
+
 $(document).ready(function(){
-    $('#form-add').on("submit", function(){
+    
+    $('#form-add').on("submit", function(e){
+        e.preventDefault()
         check_add()
+        draw_table()
     })
-    $('#form-rm').on("submit", function(){
+    $('#form-rm').on("submit", function(e){
+        e.preventDefault()
         check_rm()
+        draw_table()
     })
 })
